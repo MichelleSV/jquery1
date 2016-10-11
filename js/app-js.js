01808f9a723fa0450e19797f6f1af4acd7c7ca67
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-10-11 12:45:41 -0500

    subiendo version js

---
+++ js/app-js.js
@@ -0,0 +1,11 @@
+window.addEventListener("load",cargar);
+function cargar() {
+	var links = document.getElementsByTagName("a");
+	for(var i=0; i<links.length;i++){
+		var url = links[i].getAttribute("href");
+		var imgYoutube = youtube.generateThumbnailUrl(url);
+		var imgNueva = document.createElement("img");
+		imgNueva.setAttribute("src", imgYoutube);
+		links[i].appendChild(imgNueva);
+	}
+}
